5941def3efc3e795a8845823c0e5258b5c527371
Corey <coreystevensnj@gmail.com>
2022-02-09 21:34:40 -0500

    First commit

---
+++ tests/niftypay.ts
@@ -0,0 +1,17 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { Niftypay } from '../target/types/niftypay';
+
+describe('niftypay', () => {
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.Niftypay as Program<Niftypay>;
+
+  it('Is initialized!', async () => {
+    // Add your test here.
+    const tx = await program.rpc.initialize({});
+    console.log("Your transaction signature", tx);
+  });
+});
