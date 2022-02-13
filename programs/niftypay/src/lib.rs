9e3f4e6d750cbc693a7cf151fda9d263cd89be46
Corey Stevens <96851016+coreyman11@users.noreply.github.com>
2022-02-13 12:22:22 -0500

    Update lib.rs

--- programs/niftypay/src/lib.rs
+++ programs/niftypay/src/lib.rs
@@ -6,7 +6,7 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod niftypay {
     use super::*;
-    pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
         Ok(())
     }
 }
